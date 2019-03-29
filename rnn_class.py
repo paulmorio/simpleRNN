1a8e894634bc80efbb708ffe868e8a8ec5d2cffd
Paul Scherer <pms69@cam.ac.uk>
2019-03-29 16:18:10 +0000

    Sampling method added

--- rnn_class.py
+++ rnn_class.py
@@ -33,19 +33,23 @@ class SimpleRNN(object):
 	Uses standard backpropagation to compute learning gradients and tuned by gradient
 	descent
 	"""
-	def __init__(self, text_df, hidden_layer_size, sequence_length, learning_rate):
+	def __init__(self, text_df, hidden_layer_size, sequence_length, learning_rate=0.01):
 		### Data given to the model ###
 		self.data = open(text_df, "r").read()
 		self.vocab = list(set(data))
 		self.vocab_size = len(vocab)
 
 		### Parameters of the model (ie weights and biases) ###
-		self.W_xh = np.random.randn(hidden_layer_size, vocab_size)*0.01 # weights from input into hidden
+		self.W_xh = np.random.randn(hidden_layer_size, self.vocab_size)*0.01 # weights from input into hidden
 		self.b_h = np.zeros((hidden_layer_size,1)) # bias of hidden layer
 		self.W_hh = np.random.randn((hidden_layer_size, hidden_layer_size)) # weights from hidden to hidden
 		# no bias from hidden to hidden loop
-		self.W_hy = np.random.randn((vocab_size, hidden_layer_size))*0.01# weights from hidden to output
-		self.b_y = np.zeros((vocab_size,1))
+		self.W_hy = np.random.randn((self.vocab_size, hidden_layer_size))*0.01# weights from hidden to output
+		self.b_y = np.zeros((self.vocab_size,1))
+
+		### Hidden state memory ###
+		self.hidden_state_memory = {}
+		self.hidden_state_memory[-1] = np.zeros((hidden_size,1)) # initial first memory
 
 		### Hyperparameters ###
 		self.hidden_layer_size = hidden_layer_size
@@ -58,7 +62,77 @@ class SimpleRNN(object):
 		self.ix_to_char = {i:ch for i, ch in enumerate(vocab)}
 
 
-	def feedforward(self, a):
+	def feedforward(self, a, t):
 		"""
-		Return the output vector if "a" is given as input 
+		Return the output vector if the "a" vector is given as input.
+
+		Params:
+		a: array-like denoting the input into the RNN.
+			Needs to be the size of the first (input) layer of the Network
+		t: time or position in sequence
 		"""
+		hidden_state = tanh(np.dot(W_xh, a) + np.dot(W_hh, hidden_state_memory[t-1]))
+		self.hidden_state_memory[t] = hidden_state # update our memory of the hidden state at time t
+		output = np.dot(W_hy, hidden_state)
+		return output
+
+	def SGD(self, epochs, test=False):
+		"""
+		Stochastic gradient descent training RNN for the sequence starting from position pos
+		in the data
+		"""
+		inputs = [self.char_to_ix[ch] for ch in self.data[pos:pos+seq_length]]
+		targets = [self.char_to_ix[ch] for ch in self.data[pos+1:pos+seq_length+1]]
+
+	def sample(self, seed_char, n):
+		"""
+		Sample n letters starting from a seed character from the RNN and print to STDOut
+		"""
+		sampled_ixs = []
+		x = self.make_one_hot_enc(seed_char)
+
+		for t in xrange(n):
+			# Predict the next letter
+			most_recent_memory_t = max(self.hidden_state_memory.keys())
+			h = tanh(np.dot(self.W_xh, x) + np.dot(self.W_hh, self.hidden_state_memory[most_recent_memory_t]))
+			y = np.dot(self.W_hy, h) + self.b_y
+			y_probs = np.exp(y)/np.sum(np.exp(y)) # Softmax
+			predicted_ix = np.random.choice(range(self.vocab_size), p=p.ravel())
+			sampled_ixs.append(predicted_ix)
+
+			# next input
+			x = np.zeros((self.vocab_size, 1))
+			x[predicted_ix] = 1
+
+		sample_text = "".join(self.ix_to_char[ix] for ix in sampled_ixs)
+		print "-------------\n %s \n---------------" % (sample_text)
+
+
+	def make_one_hot_enc(self, char):
+		"""
+		Returns a one hot encoding of the character given the models vocab
+		"""
+		x = np.zeros((self.vocab_size, 1))
+		seed_index = char_to_ix[seed_char]
+		x[seed_index] = 1
+		return x
+
+
+
+
+
+
+
+# Static Functions
+##################
+def tanh(z):
+	"""
+	Returns the element-wise tanh at z
+	"""
+	return (np.tanh(z))
+
+def tanh_prime(z):
+	"""
+	Returns the element-wise derivative of tanh at z
+	"""
+	return (1.0 - np.tanh(z)**2)
